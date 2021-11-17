43474bd7d950d9afd58e42577458244613da88fb
emmlan49 <emmanuel.landais49@gmail.com>
2021-11-17 14:13:07 +0100

    creation composant Buton simple

---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Button.css"
+
+/**
+ * Composant Button HTML
+ * @param {object} props props du composant Button 
+ * @returns component render
+ */
+const Button = (props) => {
+  return <button className="Button">Benjamin Button</button>;
+};
